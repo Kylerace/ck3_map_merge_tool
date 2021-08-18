ba90b1115b041c0b9b58d9d7475ae4963416dbd4
iamgoofball <iamgoofball@gmail.com>
2021-08-18 14:19:35 -0700

    I'm an idiot, sorry!!!

--- step_2_register_changes.py
+++ step_2_register_changes.py
@@ -9,62 +9,62 @@ from PIL import ImageFile
 ImageFile.LOAD_TRUNCATED_IMAGES = True
 
 if __name__ == "__main__":
-    # changed_pixels = {}
-    # print("Loading heightmap backup...")
-    # original_heightmap = Image.open("./temp/step1_backup/heightmap.png", "r")
-    # print("Iterating over heightmap and registering changes...")
-    # heightmap = Image.open("../map_data/heightmap.png", "r")
-    # width, height = heightmap.size
-    # print(width)
-    # print(height)
-    # for x in tqdm(range(width)):
-        # for y in range(height):
-            # heightmap_height = heightmap.getpixel((x, y))
-            # o_height = original_heightmap.getpixel((x, y))
-            # if heightmap_height != o_height:
-                # changed_pixels[(x, y)] = heightmap_height
-    # print("Found " + str(len(changed_pixels)) + " changed pixels.")
-    # print("Pickling the changes...")
-    # with open('./temp/step2_backup/heightmap.pickle', 'wb') as heightmap_pickle:
-        # pickle.dump(changed_pixels, heightmap_pickle, protocol=pickle.HIGHEST_PROTOCOL)
+    changed_pixels = {}
+    print("Loading heightmap backup...")
+    original_heightmap = Image.open("./temp/step1_backup/heightmap.png", "r")
+    print("Iterating over heightmap and registering changes...")
+    heightmap = Image.open("../map_data/heightmap.png", "r")
+    width, height = heightmap.size
+    print(width)
+    print(height)
+    for x in tqdm(range(width)):
+        for y in range(height):
+            heightmap_height = heightmap.getpixel((x, y))
+            o_height = original_heightmap.getpixel((x, y))
+            if heightmap_height != o_height:
+                changed_pixels[(x, y)] = heightmap_height
+    print("Found " + str(len(changed_pixels)) + " changed pixels.")
+    print("Pickling the changes...")
+    with open('./temp/step2_backup/heightmap.pickle', 'wb') as heightmap_pickle:
+        pickle.dump(changed_pixels, heightmap_pickle, protocol=pickle.HIGHEST_PROTOCOL)
         
-    # changed_pixels = {}
-    # print("Loading detail_index backup...")
-    # original_heightmap = Image.open("./temp/step1_backup/detail_index.tga", "r")
-    # print("Iterating over detail_index and registering changes...")
-    # heightmap = Image.open("../gfx/map/terrain/detail_index.tga", "r")
-    # width, height = heightmap.size
-    # print(width)
-    # print(height)
-    # for x in tqdm(range(width)):
-        # for y in range(height):
-            # heightmap_height = heightmap.getpixel((x, y))
-            # o_height = original_heightmap.getpixel((x, y))
-            # if heightmap_height != o_height:
-                # changed_pixels[(x, y)] = heightmap_height
-    # print("Found " + str(len(changed_pixels)) + " changed pixels.")
-    # print("Pickling the changes...")
-    # with open('./temp/step2_backup/detail_index.pickle', 'wb') as detailindex_pickle:
-        # pickle.dump(changed_pixels, detailindex_pickle, protocol=pickle.HIGHEST_PROTOCOL)
+    changed_pixels = {}
+    print("Loading detail_index backup...")
+    original_heightmap = Image.open("./temp/step1_backup/detail_index.tga", "r")
+    print("Iterating over detail_index and registering changes...")
+    heightmap = Image.open("../gfx/map/terrain/detail_index.tga", "r")
+    width, height = heightmap.size
+    print(width)
+    print(height)
+    for x in tqdm(range(width)):
+        for y in range(height):
+            heightmap_height = heightmap.getpixel((x, y))
+            o_height = original_heightmap.getpixel((x, y))
+            if heightmap_height != o_height:
+                changed_pixels[(x, y)] = heightmap_height
+    print("Found " + str(len(changed_pixels)) + " changed pixels.")
+    print("Pickling the changes...")
+    with open('./temp/step2_backup/detail_index.pickle', 'wb') as detailindex_pickle:
+        pickle.dump(changed_pixels, detailindex_pickle, protocol=pickle.HIGHEST_PROTOCOL)
         
-    # changed_pixels = {}
-    # print("Loading detail_intensity backup...")
-    # original_heightmap = Image.open("./temp/step1_backup/detail_intensity.tga", "r")
-    # print("Iterating over detail_intensity and registering changes...")
-    # heightmap = Image.open("../gfx/map/terrain/detail_intensity.tga", "r")
-    # width, height = heightmap.size
-    # print(width)
-    # print(height)
-    # for x in tqdm(range(width)):
-        # for y in range(height):
-            # heightmap_height = heightmap.getpixel((x, y))
-            # o_height = original_heightmap.getpixel((x, y))
-            # if heightmap_height != o_height:
-                # changed_pixels[(x, y)] = heightmap_height
-    # print("Found " + str(len(changed_pixels)) + " changed pixels.")
-    # print("Pickling the changes...")
-    # with open('./temp/step2_backup/detail_intensity.pickle', 'wb') as detail_intensity_pickle:
-        # pickle.dump(changed_pixels, detail_intensity_pickle, protocol=pickle.HIGHEST_PROTOCOL)
+    changed_pixels = {}
+    print("Loading detail_intensity backup...")
+    original_heightmap = Image.open("./temp/step1_backup/detail_intensity.tga", "r")
+    print("Iterating over detail_intensity and registering changes...")
+    heightmap = Image.open("../gfx/map/terrain/detail_intensity.tga", "r")
+    width, height = heightmap.size
+    print(width)
+    print(height)
+    for x in tqdm(range(width)):
+        for y in range(height):
+            heightmap_height = heightmap.getpixel((x, y))
+            o_height = original_heightmap.getpixel((x, y))
+            if heightmap_height != o_height:
+                changed_pixels[(x, y)] = heightmap_height
+    print("Found " + str(len(changed_pixels)) + " changed pixels.")
+    print("Pickling the changes...")
+    with open('./temp/step2_backup/detail_intensity.pickle', 'wb') as detail_intensity_pickle:
+        pickle.dump(changed_pixels, detail_intensity_pickle, protocol=pickle.HIGHEST_PROTOCOL)
         
         
     print("Iterating over masks...")
